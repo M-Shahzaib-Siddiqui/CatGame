d7337e9c26bc025e3553d1a64e9cf4a0d5b1a401
Shahzaib Siddiqui <123213560+M-Shahzaib-Siddiqui@users.noreply.github.com>
2024-07-04 14:32:50 -0400

    implement health bar + healing system + add player sprite

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,17 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public float health;
+    public float health, timeBeforeHeal;
+    private float maxHealth, lastHit;
+    public Image healthBar;
+
+    void Start() {
+        maxHealth = health;
+    }
+
+    void Update() {
+        if (Time.time>lastHit+timeBeforeHeal && health<maxHealth) {
+            HealthUpdate(1);
+        }
+    }
 
     void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.GetComponent<EnemyDamage>() != null) {
             GameObject enemy = collision.gameObject;
-            health -= enemy.GetComponent<EnemyDamage>().damage;
+            HealthUpdate(enemy.GetComponent<EnemyDamage>().damage*-1);
+            lastHit = Time.time;
         }
 
         if (health<=0) {Destroy(gameObject);}
+    }
+
+    void HealthUpdate(float changeInHealth) {
+        health += changeInHealth;
+        healthBar.fillAmount = health / maxHealth;
     }
 }
